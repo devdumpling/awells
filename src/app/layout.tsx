57b8dd72acc475b1f817ae3bcc34f4e0c7af39fe
Devon Wells <devon.wells@pm.me>
2024-03-30 09:19:18 -0400

    init shad, fix nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+import "@/styles/globals.css";
 import { Nav } from "@/components/Nav";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "awells.dev",
@@ -10,8 +17,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("bg-background min-h-screen font-sans antialiased", fontSans.variable)}>
         <Nav />
         {children}
       </body>
