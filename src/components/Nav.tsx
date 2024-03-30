e957a0de1562e62ece011e560adad9ee907d1606
Devon Wells <devon.wells@pm.me>
2024-03-30 14:50:13 -0400

    more play

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export function Nav() {
   return (
     <nav>
-      <div className="flex h-14 items-center justify-between px-4 md:px-6">
-        <Link className="font-semibold" href="#">
-          Devon Wells
+      <div className="p-4 md:p-6">
+        <Link className="font-mono font-semibold" href="/">
+          /dev.wells
         </Link>
         <nav className="flex gap-4 text-sm font-medium">
-          <Link href="/blog">Blog</Link>
-          <Link href="/about">About</Link>
-          <Link href="/work">Work</Link>
+          <Link href="/blog">/blg</Link>
+          <Link href="/about">/abt</Link>
+          <Link href="/work">/wrk</Link>
         </nav>
       </div>
     </nav>
